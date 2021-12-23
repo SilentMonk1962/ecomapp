ac023673664781377a4392dbd6fcf61f1e8b6132
silentmonk1962 <abhishek.singh@iiml.org>
2021-12-23 10:17:24 +0530

    added shop page and components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.components';
+const HatsPage =()=>(
+  <div>
+    <h1>
+      Hats Page Bitches
+    </h1>
+  </div>
+);
 
 function App(){
-  return <div>
-    <HomePage/>
+  return ( <div>
+    <Routes>
+    <Route path = '/' element={<HomePage/>}/>
+    <Route path = '/hats' element={<HatsPage/>}/>
+    <Route path='/shop' element={<ShopPage/>}/>
+    </Routes>
   </div>
-  
+  )
 }
 
 export default App;
