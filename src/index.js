8efc03f784dffb1744330bf361aa0983541ea67a
silentmonk1962 <abhishek.singh@iiml.org>
2021-12-29 07:55:13 +0530

    shopping cart svg addded

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(
+<Provider>
+<BrowserRouter>
 <App />
 </BrowserRouter>
+</Provider>
 , document.getElementById('root'));
