aa5f83ea468c7ad81778a1f5ad76d7c3ea161bf1
silentmonk1962 <abhishek.singh@iiml.org>
2021-12-29 09:08:05 +0530

    cart icon added and cart dropdown added to our application, also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,18 +4,19 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth } from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
-const Header = ({currentUser})=>(
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+const Header = ({currentUser,hidden})=>(
     <div className='header'>
         <Link className ='logo-container'to="/">
         <Logo className='logo'/>
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
-        </div>
-        <div className='options'>
+        
+        
             <Link className='option' to='/shop'>CONTACT</Link>
-        </div>
-        <div className='options'>
             
             {
                 //checks if current object is true, if yes, then displays SignOut else SignIn
@@ -24,12 +25,16 @@ const Header = ({currentUser})=>(
                 :
                 <Link className='option' to='/signin'>SIGNIN</Link>
             }
+            <CartIcon/>
+            {hidden? null :<CartDropdown/>}
         </div>
 
     </div>
 );
 //gets the value of the currentUser object from the state and then passes it as prop to the reducer
-const mapStateToProps =state=>({
-    currentUser:state.user.currentUser
+const mapStateToProps =({user:{currentUser},cart:{hidden}})=>({
+    currentUser:currentUser,
+    hidden:hidden
+
 })
 export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-<<<<<<< HEAD
-
-ReactDOM.render(
-<Provider>
-=======
 import store from './redux/store';
 
 ReactDOM.render(
 <Provider store={store}>
->>>>>>> 542765ea55f1efbc0403506a17b25d83c95ac890
 <BrowserRouter>
 <App />
 </BrowserRouter>
