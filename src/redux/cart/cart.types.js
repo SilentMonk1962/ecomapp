8b552e592cb47b03eb220c9646c381fe00cb4fd4
silentmonk1962 <abhishek.singh@iiml.org>
2021-12-29 10:20:27 +0530

    added cart reducer

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,7 @@
 const cartActionTypes={
     TOGGLE_CART_HIDDEN:'TOGGLE_CART_HIDDEN',
-    ADD_TO_CART:'ADD_TO_CART',
-    REMOVE_FROM_CART:'REMOVE_FROM_CART'
+    ADD_ITEM:'ADD_ITEM',
+    REMOVE_ITEM:'REMOVE_ITEM'
 }
 
 export default cartActionTypes;
